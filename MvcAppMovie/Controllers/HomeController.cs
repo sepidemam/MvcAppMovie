0501b29724eb99673b6f1eb37fb4902342171720
sepideh <emam@ualberta.ca>
2014-03-13 13:05:08 -0500

    commit1

---
+++ MvcAppMovie/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcAppMovie.Models;
+
+namespace MvcAppMovie.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        MoviesDBEntities _db;
+
+        public HomeController()
+        {
+            _db = new MoviesDBEntities();
+        }
+
+
+        public ActionResult Index()
+        {
+            ViewData.Model = _db.TableMovies;
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcAppMovie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAppMovie.MvcApplication" Language="C#" %>
